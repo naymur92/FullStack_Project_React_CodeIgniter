d7547f7cddf40ad5bf7b6afa4b892223675ce1c7
naymur92 <naymurrahman@yahoo.com>
2023-01-07 18:20:17 +0600

    usersseeder updated

--- backend/app/Database/Seeds/UsersSeeder.php
+++ backend/app/Database/Seeds/UsersSeeder.php
@@ -2,83 +2,84 @@
 
 namespace App\Database\Seeds;
 
+use App\Models\UsersModel;
 use CodeIgniter\Database\Seeder;
 
 class UsersSeeder extends Seeder
 {
   public function run()
   {
-    $users = [
+    $model = new UsersModel();
+    $model->insertBatch([
       [
         'name' => 'Naymur Rahman',
         'email' => 'naymur@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'admin',
         'status' => 'active',
       ],
       [
         'name' => 'Kamrul Hasan',
         'email' => 'kamrul@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'manager',
         'status' => 'active',
       ],
       [
         'name' => 'Alauddin Alo',
         'email' => 'alo@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'active',
       ],
       [
         'name' => 'Amjad Hossain',
         'email' => 'amjad@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'active',
       ],
       [
         'name' => 'Fayzullah Aman',
         'email' => 'fayzullah@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'active',
       ],
       [
         'name' => 'Ahmad Sharif',
         'email' => 'ahmed@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'pending',
       ],
       [
         'name' => 'Farhad Hossain',
         'email' => 'farhad@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'manager',
         'status' => 'pending',
       ],
       [
         'name' => 'Jamil Hossain',
         'email' => 'jamil@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'active',
       ],
       [
         'name' => 'Abu Syed',
         'email' => 'syed@example.com',
-        'password' => 'abcd1234',
+        'password' => password_hash("abcd1234", PASSWORD_DEFAULT),
         'role' => 'user',
         'status' => 'pending',
       ],
 
-    ];
+    ]);
 
     // Simple Queries
-    foreach ($users as $user) {
-      $hashedpassword = password_hash($user['password'], PASSWORD_DEFAULT);
-      $this->db->query("INSERT INTO users VALUES(NULL, :name:, :email:, '$hashedpassword', :role:, :status:, DEFAULT)", $user);
-    }
+    // foreach ($users as $user) {
+    //   $this->db->query("INSERT INTO users VALUES(NULL, :name:, :email:, :password:, :role:, :status:, DEFAULT)", $user);
+    // }
   }
 }
